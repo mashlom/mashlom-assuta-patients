f223f14d692629c48e9763147aaf1b264fdc563b
relbns <ariel@stoketalent.com>
2024-06-29 23:43:24 +0300

    Change 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -8,7 +8,7 @@ export default function NotFoundPage() {
     <Layout includeCover={false}>
       <h1>Page not found - 404</h1>
       <p>
-        You just hit a route that doesn&#39;t exist... the sadness.
+        You just hit a route that doesn&#39;t exist...
         <br />
         <Link to="/">Go home</Link>.
       </p>
